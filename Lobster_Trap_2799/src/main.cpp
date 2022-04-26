460051c007ed449be1ea819cbac109d5b5d4c395
Slater-C <scampbell@wpi.edu>
2022-04-25 23:33:37 -0400

    rotary encoder button poop

--- Lobster_Trap_2799/src/main.cpp
+++ Lobster_Trap_2799/src/main.cpp
@@ -24,7 +24,8 @@ int val = 1;
 bool on = true;
 
 DFRobot_LCD lcd(16,2);  //16 characters and 2 lines of show
-char modeState = 2;
+char modeState = 0;
+char inputState = 0;
 long epoch_time = 1650421475; // Placeholder
 Bounce sw = Bounce();
 
@@ -46,32 +47,72 @@ void loop() {
     //breath(REG_ONLY);
 
 	switch(modeState){
-		case 0: {	// Demo modeState
-			
-			sw.update();
-			long encoderVal = read_encoder_value();
-			static int i = 0;
-			if(i > 100){
-				time_t rawtime = epoch_time + encoderVal;
-				struct tm *info;
-				char buffer[17];
-				char buffer2[24];
-				time( &rawtime );
-				info = localtime( &rawtime );
-				
-				lcd.setCursor(0, 0);
-				strftime(buffer, 17,"%H:%M:%S %b %d", info);
-				lcd.print(buffer);
-				lcd.setCursor(0, 1);
-				sprintf(buffer2, "R_Encoder: %ld      ", encoderVal);
-				lcd.print(buffer2);
-				i = 0;
+		case 0: {	// User input modeState
+			switch(inputState){
+				static long millisDelay = 0;
+				case 0: {	// Rotary input
+					sw.update();
+					long encoderVal = read_encoder_value();
+					static int i = 0;
+					if(i > 100){
+						time_t rawtime = epoch_time + encoderVal;
+						struct tm *info;
+						char buffer[17];
+						char buffer2[24];
+						time( &rawtime );
+						info = localtime( &rawtime );
+						
+						lcd.setCursor(0, 0);
+						strftime(buffer, 17,"%H:%M:%S %b %d", info);
+						lcd.print(buffer);
+						lcd.setCursor(0, 1);
+						sprintf(buffer2, "R_Encoder: %ld      ", encoderVal);
+						lcd.print(buffer2);
+						i = 0;
+					}
+					i++;
+					if(sw.rose()){		// rose should be on release
+						//inputState = 1;
+						millisDelay = millis();
+					}
+				}
+				case 1: {	// Time confirmation
+					sw.update();
+					if((millis() - millisDelay) < 2000){
+						lcd.setCursor(0, 0);
+						lcd.print("Press to confirm");
+					}
+					else{
+						lcd.setCursor(0, 0);
+						lcd.print("Wait to cancel");
+					}
+					lcd.setCursor(0, 1);
+					lcd.print("04:20:00 4/20");	// Programmed release time
+
+					if((millis() - millisDelay) > 5000){
+						inputState = 0;
+					}
+
+					if(sw.rose()){
+						inputState = 2;
+					}
+				}
+				case 2: {	// Proceed to underwater
+					lcd.setCursor(0, 0);
+					lcd.print("Release confirmd");
+					lcd.setCursor(0, 1);
+					lcd.print("Extending magnet");
+					inputState = 0;
+					modeState = 0;
+
+				}
+
 			}
 
 
-			i++;
+			
 		}
-		case 1: { // User input modeState
+		case 1: { // Motor extend modeState
 			bool switchState1 = digitalRead(7);
 			bool switchState2 = digitalRead(1);
 			
@@ -118,6 +159,14 @@ void loop() {
 			}
 		}
 		case 3: { // Release idle modeState
+			sw.update();
+			lcd.setCursor(0, 0);
+			static int count = 0;
+			if(sw.rose()){
+				count++;
+			}
+			lcd.print(count);
+
 
 		}
 		// default: {
